8d99bc74366cb8ea3d574f63eb02b7f885b6a623
Rence <itsrealflorence@gmail.com>
2025-07-22 00:04:56 +0800

    exer 1-6

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,10 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    for(let i = num1; i < num2; num1++){
+        sum += num1;
+    }
+
+    return sum;
 
 };
 
